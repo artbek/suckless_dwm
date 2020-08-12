7ba947ff5ef3a8ef1c6b93c755003526a08de32b
artbek <bart@artbek.co.uk>
2020-08-12 23:58:32 +0100

    [Special Keys]: volume & backlight.

--- config.h
+++ config.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#include "X11/XF86keysym.h"
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -58,6 +60,10 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *upvol[]    = { "amixer", "set", "Master", "2%+", NULL };
+static const char *downvol[]  = { "amixer", "set", "Master", "2%-", NULL };
+static const char *upbrightness[]   = { "xbacklight", "-inc", "2", NULL };
+static const char *downbrightness[] = { "xbacklight", "-dec", "2", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -94,6 +100,10 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ 0,         XF86XK_AudioRaiseVolume,     spawn,           {.v = upvol } },
+	{ 0,         XF86XK_AudioLowerVolume,     spawn,           {.v = downvol } },
+	{ 0,          XF86XK_MonBrightnessUp,     spawn,           {.v = upbrightness } },
+	{ 0,        XF86XK_MonBrightnessDown,     spawn,           {.v = downbrightness } },
 };
 
 /* button definitions */
